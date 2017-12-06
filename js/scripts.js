c8d796aca1a3ba94e2487f04bc5e3eed35a58968
Vikaras <arturasch@gmail.com>
2017-12-06 16:27:14 +0200

    Gallery filters still in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -101,3 +101,24 @@ function onScroll(event){
     alert('Thank you for your question!')
   })
 })()
+
+// gallery filter
+
+jQuery('document').ready( function($) {
+  $('.gallery-filters li').on('click', function() {
+    $('.gallery-filters li').removeClass('active');
+    $(this).addClass('active');
+    filter = $(this).attr('gallery-filter');
+
+    $('.gallery-column').each( function() {
+      if (filter == 'all') {
+        $(this).fadeIn();
+      } else {
+        $(this).hide();
+        if ($(this).attr('filter-category') == filter) {
+          $(this).fadeIn();
+        }
+      }
+    });
+  });
+});
