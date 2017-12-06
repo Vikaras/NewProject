b30d64c286be866199e9132af79fc2f83f001829
Vikaras <arturasch@gmail.com>
2017-12-06 13:38:10 +0200

    Added active links in website menu, different hovers and JS with smooth scroll and active.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Google maps JS
 function initMap() {
   var uluru = {lat: 51.3450, lng: 0.1317}
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -10,6 +11,45 @@ function initMap() {
   })
 }
 
+// Carousel Js
 $(document).ready(function(){
   $('.slider').bxSlider();
+
+  $(document).on("scroll", onScroll);
+
+  //smoothscroll
+  $('.nav-list li a').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+
+    var target = this.hash,
+      menu = target;
+
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+  });
 });
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.nav-list li a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.outerHeight(true) > scrollPos) {
+      $('.nav-list li a').removeClass("active");
+      currLink.addClass("active");
+    } else {
+      currLink.removeClass("active");
+    }
+  });
+}
